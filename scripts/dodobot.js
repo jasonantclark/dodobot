da866757d4918097496bd07e79dfc4f57c497390
Jason Clark <jason.clark@laterooms.com>
2016-04-20 17:24:18 +0100

    instance brackets?

--- scripts/dodobot.js
+++ scripts/dodobot.js
@@ -1,12 +1,11 @@
 module.exports = function(robot) {
    robot.respond(/\btaming\b ([1-9][0-9]{0,2}) (.*)/i, function(msg){
-         var url = "http://www.dododex.com/taming/"+msg.match[2]+"/"+msg.match[1]
-
+        var url = "http://www.dododex.com/taming/"+msg.match[2]+"/"+msg.match[1]
         var phantom = require('phantom');
         var sitepage = null;
         var phInstance = null;
         phantom.create()
-            .then(instance => {
+            .then((instance) => {
                 phInstance = instance;
                 return instance.createPage();
             })
