9c36f8b6fae1624f8d60d9935f6103f170d3ca2f
Jason Clark <jason.clark@laterooms.com>
2016-04-20 15:19:09 +0100

    added some stuff to pull page title

--- scripts/dodobot.js
+++ scripts/dodobot.js
@@ -1,5 +1,8 @@
 module.exports = function(robot) {
     robot.respond(/\bWhat do I need to tame a level\b ([1-9][0-9]{0,2}) (.*)/i, function(msg){
-       msg.reply(msg.match[1]);
+      var url = "http://www.dododex.com/taming/"+msg.match[2]+"/"+msg.match[1]+"/" 
+			page.open(url, function (status) {
+			phantom.exit();			
+       msg.reply(page.title);
     });
 }
