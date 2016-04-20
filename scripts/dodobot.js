ea9d53d5dd892943fa1daf4568235abac1afa108
Danny Parkinson <danny.parkinson@laterooms.com>
2016-04-20 16:07:23 +0100

    phantomjs stuff

--- scripts/dodobot.js
+++ scripts/dodobot.js
@@ -1,8 +1,32 @@
+
+var phantom = require('phantom');
 module.exports = function(robot) {
-    robot.respond(/\bWhat do I need to tame a level\b ([1-9][0-9]{0,2}) (.*)/i, function(msg){
-      var url = "http://www.dododex.com/taming/"+msg.match[2]+"/"+msg.match[1]+"/" 
-			page.open(url, function (status) {
-			phantom.exit();			
-       msg.reply(page.title);
+    robot.respond(/\btaming\b ([1-9][0-9]{0,2}) (.*)/i, function(msg){
+      	var url = "http://www.dododex.com/taming/"+msg.match[2]+"/"+msg.match[1]
+		var sitepage = null;
+		var phInstance = null;
+		phantom.create()
+		    .then(instance => {
+		        phInstance = instance;
+		        return instance.createPage();
+		    })
+		    .then(page => {
+		        sitepage = page;
+		        return page.open(url);
+		    })
+		    .then(status => {
+		        return sitepage.property('title');
+		    })
+		    .then(content => {
+		    	msg.reply(content);
+		        sitepage.close();
+		        phInstance.exit();
+		    })
+		    .catch(error => {
+		        phInstance.exit();
+		    });
     });
 }
+
+
+
