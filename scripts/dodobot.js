dbe382bb2ee31aed8ea8f61f8ccbabe5d52aeff8
Jason Clark <jason.clark@laterooms.com>
2016-04-20 14:28:19 +0100

    testing var

--- scripts/dodobot.js
+++ scripts/dodobot.js
@@ -1,5 +1,5 @@
 module.exports = function(robot) {
     robot.respond(/\bWhat do I need to tame a level\b ([0-9]|1[0-9][0-9]) (.*)/i, function(msg){
-       msg.reply("test");
+       msg.reply(i[0]);
     });
 }
