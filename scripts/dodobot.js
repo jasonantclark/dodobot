557ef9f48cc7a5cb34314b503ab49f3f3f31b266
Danny Parkinson <danny.parkinson@laterooms.com>
2016-04-20 17:38:51 +0100

    different syntax for dodobot script

--- scripts/dodobot.js
+++ scripts/dodobot.js
@@ -1,32 +1,43 @@
-
-var phantom = require('phantom');
 module.exports = function(robot) {
     robot.respond(/\btaming\b ([1-9][0-9]{0,2}) (.*)/i, function(msg){
-      	var url = "http://www.dododex.com/taming/"+msg.match[2]+"/"+msg.match[1]
-		var sitepage = null;
-		var phInstance = null;
-		phantom.create()
-		    .then(instance => {
-		        phInstance = instance;
-		        return instance.createPage();
-		    })
-		    .then(page => {
-		        sitepage = page;
-		        return page.open(url);
-		    })
-		    .then(status => {
-		        return sitepage.property('title');
-		    })
-		    .then(content => {
-		    	msg.reply(content);
-		        sitepage.close();
-		        phInstance.exit();
-		    })
-		    .catch(error => {
-		        phInstance.exit();
-		    });
+        var url = "http://www.dododex.com/taming/"+msg.match[2]+"/"+msg.match[1];
+
+        var phantom = require('phantom');
+        var sitepage = null;
+        var phInstance = null;
+
+        var url = "http://www.dododex.com/taming/"+msg.match[2]+"/"+msg.match[1];
+        phantom.create()
+            .then(function(instance) {
+                phInstance = instance;
+                return instance.createPage();
+            })
+            .then(function(page) {
+                sitepage = page;
+                return page.open(url);
+            })
+            .then(function(status) {
+                return sitepage.property('title');
+            })
+            .then(function(content) {
+                msg.reply(content);
+                sitepage.close();
+                phInstance.exit();
+            })
+            .catch(function(error) {
+                phInstance.exit();
+            });
+
     });
 }
 
 
 
+
+
+/*
+
+        
+
+
+            */
