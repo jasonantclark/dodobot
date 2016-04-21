e2c4ea848986ba42733708a5a2eb8975f21d97c9
Danny Parkinson <danny.parkinson@laterooms.com>
2016-04-21 13:10:09 +0100

    renders taming table

--- scripts/dodobot.js
+++ scripts/dodobot.js
@@ -2,7 +2,10 @@ module.exports = function(robot) {
     robot.respond(/\btaming\b ([1-9][0-9]{0,2}) (.*)/i, function(msg){
         var url = "http://www.dododex.com/taming/"+msg.match[2]+"/"+msg.match[1];
 		var webshot = require('webshot');
-		webshot(url, 'dododex.png', function(err) {
+		var options = {
+			captureSelector: ".tamingTable"
+		};
+		webshot(url, 'dododex.png', options, function(err) {
 		});
     });
 }
