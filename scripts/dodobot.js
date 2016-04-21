8013b6030536ecbee59780861a1b3e69b38890b8
Danny Parkinson <danny.parkinson@laterooms.com>
2016-04-21 16:24:07 +0100

    hubot returning d/l image

--- scripts/dodobot.js
+++ scripts/dodobot.js
@@ -1,11 +1,31 @@
+webshot = require('webshot');
+request = require('request');
+fs = require('fs');
+
 module.exports = function(robot) {
     robot.respond(/\btaming\b ([1-9][0-9]{0,2}) (.*)/i, function(msg){
-        var url = "http://www.dododex.com/taming/"+msg.match[2]+"/"+msg.match[1];
-		var webshot = require('webshot');
+        var readurl = "http://www.dododex.com/taming/"+msg.match[2]+"/"+msg.match[1];
+        var slackapiurl = 'https://slack.com/api/files.upload'
+        var filepath = 'dododex.png'
 		var options = {
 			captureSelector: ".tamingTable"
 		};
-		webshot(url, 'dododex.png', options, function(err) {
-		});
+
+        webshot(readurl, filepath, options, function(err) {
+                var form = {
+                file: fs.createReadStream(filepath),
+                filename: filepath,
+                channels: 'testing',
+                token:"xoxb-35066962770-JSWipKypknHBSTh4i3ZWkFx4",
+                initial_comment:'All Rights Reserved dododex.com'};
+
+
+            request.post({url:slackapiurl, formData: form}, function optionalCallback(err, httpResponse, body) {
+                if (err) {
+                    return console.error('upload failed:', err);
+                }
+                console.log('Upload successful!  Server responded with:', body);
+            });
+        });
     });
 }
