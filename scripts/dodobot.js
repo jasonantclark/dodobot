228ef813550832f107fc505b31eb78033e39b634
Danny Parkinson <danny.parkinson@laterooms.com>
2016-04-21 13:07:29 +0100

    webshot

--- scripts/dodobot.js
+++ scripts/dodobot.js
@@ -1,30 +1,8 @@
 module.exports = function(robot) {
     robot.respond(/\btaming\b ([1-9][0-9]{0,2}) (.*)/i, function(msg){
         var url = "http://www.dododex.com/taming/"+msg.match[2]+"/"+msg.match[1];
-        var phantom = require('phantom');
-        var sitepage = null;
-        var phInstance = null;
-        phantom.create()
-            .then(function(instance) {
-                phInstance = instance;
-                return instance.createPage();
-            })
-            .then(function(page) {
-                sitepage = page;
-                return page.open(url);
-            })
-            .then(function(status) {
-                return sitepage.property('title');
-            })
-            .then(function(content) {
-                msg.reply(content);
-                sitepage.close();
-                phInstance.exit();
-            })
-            .catch(function(error) {
-                phInstance.exit();
-            });
-
+		var webshot = require('webshot');
+		webshot(url, 'dododex.png', function(err) {
+		});
     });
 }
-
