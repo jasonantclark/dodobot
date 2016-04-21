deab2ab2ebcd3b7ebfbf3e3b60daa4a03e735192
jasonantclark <jason-clark@outlook.com>
2016-04-21 17:19:11 +0100

    Update dodobot.js
    testing msg room...

--- scripts/dodobot.js
+++ scripts/dodobot.js
@@ -7,6 +7,7 @@ module.exports = function(robot) {
         var readurl = "http://www.dododex.com/taming/"+msg.match[2]+"/"+msg.match[1];
         var slackapiurl = 'https://slack.com/api/files.upload'
         var filepath = 'dododex.png'
+        var room = msg.message.room
 		var options = {
 			captureSelector: ".tamingTable"
 		};
@@ -15,7 +16,7 @@ module.exports = function(robot) {
                 var form = {
                 file: fs.createReadStream(filepath),
                 filename: filepath,
-                channels: 'testing',
+                channels: room,
                 token:"xoxb-35066962770-JSWipKypknHBSTh4i3ZWkFx4",
                 initial_comment:'All Rights Reserved dododex.com'};
 
